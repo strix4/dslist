9dd26ee33c03ac044368cbddb20d3f33ddaa66b0
renanfernandes <renan.alfer@gmail.com>
2024-11-16 12:04:24 -0300

    Search gameById

--- src/main/java/com/devsuperior/dslist/services/GameService.java
+++ src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -4,20 +4,32 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.entities.Game;
 import com.devsuperior.dslist.repositories.GameRepository;
+
 
 // @Component ou @Service
 @Service
 public class GameService {
 	
+//	Objeto gameRepository instânciado para realizar a busca no repositório
+//	usando findById da classe GameRepository
 	@Autowired
 	private GameRepository gameRepository;
 	
+    @Transactional(readOnly = true)
+	public GameDTO findById(Long id) {
+		Game result = gameRepository.findById(id).get();
+		return new GameDTO(result);
+	}
+	
 	// Realiza uma busca no banco de dados e retorna em forma de lista
 	// utilizando a instância do repositorio gameRepository com a função findAll 'buscar todos'
+//	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll() {
 		List<Game> result = gameRepository.findAll();
 		return result.stream().map(x -> new GameMinDTO(x)).toList();
